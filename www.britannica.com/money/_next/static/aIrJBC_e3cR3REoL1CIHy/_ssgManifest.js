b738c6562f753847363120904950addf324e5d9d
Rishul <rishulgupta16@.com>
2022-10-02 05:19:12 -0700

    first commit

---
+++ www.britannica.com/money/_next/static/aIrJBC_e3cR3REoL1CIHy/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F[article]","\u002Fabout","\u002Fauthor\u002F[authorSlug]\u002F[authorId]","\u002Fhousehold-finance","\u002Finvesting","\u002Fretirement"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
